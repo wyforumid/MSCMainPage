28f2689a0490ce6b07af0574e7eba6d60417b447
unknown <jasoliu@cnszx-wst078win.chn.msc-asia.net>
2014-11-11 17:27:24 +0800

    add group step 1 is ok, working on step 2.

---
+++ API/permissionAPI.js
@@ -0,0 +1,21 @@
+var db = require('../DB/dbutil');
+var Parameter = require('../DB/parameter');
+var mssql = require('mssql');
+
+exports.getContainGroupName = function(info,callback){
+	var params = [];
+	if(!info || info.length ==0){
+		return null;
+	}else{
+		var t = new mssql.Table();
+		t.columns.add('OfficeId',mssql.Int);
+		t.columns.add('DeptId',mssql.Int);
+
+		for(var i = info.length;i--;){
+			t.rows.add(info[i].officeId,info[i].deptId);
+		}
+
+		params.push(new Parameter('info',mssql.TVP,t));
+		db.querySP('SP_RegistUser',params,callback);
+	}
+}
--- public/javascripts/self-user-controllers.js
+++ public/javascripts/self-user-controllers.js
@@ -11,7 +11,8 @@ angular.module('selfControllers',['selfServices'])
 	$scope.company = {
 		offices:[],
 		depts:[]
-	}
+	};
+	$scope.feedbackInfo = [];
 
 	fundationService.getAllOffices(
 		function(data){
@@ -37,41 +38,91 @@ angular.module('selfControllers',['selfServices'])
 	});
 
 	$scope.registUser = function(){
+		$('#registBtn').prop('disabled',true);
+		$scope.feedbackInfo = [];
+		initialCSS();
+
+		if(!checkEssentialInfo()){
+			return;
+		}
+
 		$http({
 			method:'POST',
 			url:'/API/user',
 			data:$scope.registUserInfo
 		}).success(function(data,status){
 			//alert(data);
+			$('#registBtn').prop('disabled',false);
 			if(data && data[0]){
 				feedbackRegistPage(data[0].info);
 			}else{
 
 			}
 		}).error(function(data,status){
+			$('#registBtn').prop('disabled',false);
 			alert(data);
-		})
+		});
+	}
+
+
+	function checkEssentialInfo(){
+		var msg = 'You need enter these informations: '
+			isValid = true;
+		if(!$scope.registUserInfo.fullName){
+			isValid=false;
+			msg +='full name, ';
+		}
+		if(!$scope.registUserInfo.loginName){
+			isValid=false;
+			msg +='login name, ';
+		}
+		if(!$scope.registUserInfo.email){
+			isValid=false;
+			msg +='email, ';
+		}
+		if(!$scope.registUserInfo.office || $scope.registUserInfo.office.id == -1){
+			isValid=false;
+			msg +='office,';
+		}
+		if(!$scope.registUserInfo.dept || $scope.registUserInfo.dept.id == -1){
+			isValid=false;
+			msg += 'department.';
+		}
+		if(!isValid){
+			alert(msg);
+		}
+		return isValid;
+
 	}
 
 	function feedbackRegistPage(msg){
-		if(data[0].info=''){
+		if(msg==''){
 			alert('Regist user successfully. The password will be sent to ' + $scope.registUserInfo.email + 'in 1 minuts.');
 			return;
 		}
 
-		var str = data[0].info;
-		if(str.indexOf('login name') != -1){
+		if(msg.indexOf('login name') != -1){
 			feedbackDiv('loginNameInput');
 		}
-		if(str.indexOf('email') != -1){
+		if(msg.indexOf('email') != -1){
 			feedbackDiv('emailInput');
 		}
-		if(str.indexOf('office') != -1){
+		if(msg.indexOf('office') != -1){
 			feedbackDiv('officeSelect');
 		}
-		if(str.indexOf('department') != -1){
+		if(msg.indexOf('department') != -1){
 			feedbackDiv('deptSelect');
 		}
+		if(msg && msg.length != 0){
+			msg = msg.substr(0,msg.length-1);
+			$scope.feedbackInfo = msg.split(';');
+			if($scope.feedbackInfo.length != 0){
+				$('#errorZone').show();
+			}else{
+				$('#errorZone').hide();
+			}
+		}
+		
 
 	}
 
@@ -83,6 +134,7 @@ angular.module('selfControllers',['selfServices'])
 	function initialCSS(){
 		$('div[class*=form-group]').removeClass('has-error').removeClass('has-feedback');
 		$('span').hide();
+		$('#errorZone').hide();
 	}
 
 	function showfeedbackCSS(errInfo){
