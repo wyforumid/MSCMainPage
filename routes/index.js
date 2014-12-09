640e0be360ace0bd83a3b1c779fc76190fdcc328
tedforv <tedforv@outlook.com>
2014-12-09 17:06:38 +0800

    SO main page

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,9 @@ router.get('/main/Logout',function(req,res){
 router.get('/user',function(req,res){
 	res.render('user.html');
 })
-
+router.get('/SO',function(req,res){
+	res.render('soMain.html');
+})
 
 
 module.exports = router;
